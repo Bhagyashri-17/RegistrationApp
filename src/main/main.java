69d3c5c3c1ca549342f4d38b8871b4f8c38e097f
Bhagyashri-17 <140493046+Bhagyashri-17@users.noreply.github.com>
2024-12-02 15:10:21 +0530

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -3,9 +3,9 @@ import dao.RegistrationDAO;
 public class Main {
     public static void main(String[] args) {
         RegistrationDAO dao = new RegistrationDAO();
-        dao.createRegistration("Komal Jadhav", "komal@example.com", "2000-01-01", "1234567890", "Mumbai");
+        dao.createRegistration("Bhagyashri Jadhav", "bhagyshrijadhav71@gmail.com", "2000-07-05", "9975329387", "Bengaluru");
         dao.readRegistration(1);
-        dao.updateEmail(1, "komal.updated@example.com");
+        dao.updateEmail(1, "bhagyashrijadhav71.updated@gmail.com");
         dao.deleteRegistration(1);
     }
 }
