4f90c67758283c43d9233891acf9a7acdb5b314c
Bhagyashri-17 <140493046+Bhagyashri-17@users.noreply.github.com>
2024-12-02 14:58:57 +0530

    Update DatabaseConnection.java
    update database connections

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/RegistrationDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password";
+   private static final String URL = "jdbc:mysql://localhost:3306/RegistrationDB";
+   private static final String USER = "your_username";
+   private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
